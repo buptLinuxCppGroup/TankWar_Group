96526ff09f58a4c0d8e46e08bc1ef3e5841d6a8e
rainto96 <450490151@qq.com>
2014-12-28 01:33:37 +0800

    rainto自己用的bat脚本么么哒

---
+++ syncToVs.bat
@@ -0,0 +1,3 @@
+copy G:\TankWar_Group\*.cpp G:\fromI5\irrlicht-1.8.1\irrlicht-1.8.1\irrlicht-1.8.1\examples\02.Quake3Map\
+copy G:\TankWar_Group\*.h G:\fromI5\irrlicht-1.8.1\irrlicht-1.8.1\irrlicht-1.8.1\examples\02.Quake3Map\
+echo. & pause
